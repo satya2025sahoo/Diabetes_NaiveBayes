d77b97f4ddaba9ffcc47fb51f03c792e47f01064
satya2025sahoo <1rn21cd044.satayranjansahoo@gmail.com>
2023-11-21 04:15:26 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask,request,app,render_template
+from flask import Response
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
+scaler=joblib.load('scaler.pkl')
+model=joblib.load('gmodel1.pkl')
+
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict():
+    result=''
+
+    if request.method=='POST':
+        Pregnancies=float(request.form.get('Pregnancies'))
+        Glucose=float(request.form.get('Glucose'))
+        BloodPressure=float(request.form.get('BloodPressure'))
+        SkinThickness=float(request.form.get('SkinThickness'))
+        Insulin=float(request.form.get('Insulin'))
+        BMI=float(request.form.get('BMI'))
+        DiabetesPedigreeFunction=float(request.form.get('DiabetesPedigreeFunction'))
+        Age=float(request.form.get('Age'))
+
+        data=scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        y_pred=model.predict(data)
+
+        if y_pred[0]==1:
+            result='DIABETIC'
+        else:
+            result='NON DIABETIC'
+
+        return render_template('single_prediction.html',result=result)
+
+    else:
+        return render_template('home.html') 
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
